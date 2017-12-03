7e2fc45418b7293ab7e0211b8b4b87b7eee18ea3
John Pham <newhope11134@gmail.com>
2017-12-02 21:27:47 -0800

    skeleton set up

---
+++ README.md
@@ -0,0 +1,4 @@
+Instructions to host locally:
+
+* python -m http.server
+* Go to http://127.0.0.1:8000/
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 if (BABYLON.Engine.isSupported()) {
     var canvas = document.getElementById("renderCanvas");
+
     var engine = new BABYLON.Engine(canvas, true);
 
     var createScene = function() {
